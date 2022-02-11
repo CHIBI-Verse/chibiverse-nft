e52667ceeadd9ebdeb6b5c140b06c2b0ccb3306f
toffysoft <apiwat.r@greenmount.co.th>
2022-02-11 15:15:48 +0700

    add metadata api

--- cmd/watcher/main.go
+++ cmd/watcher/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/CHIBI-Verse/chibiverse-nft/consts"
 	"github.com/CHIBI-Verse/chibiverse-nft/services"
+	"github.com/labstack/echo/v4"
 )
+
+type HiddenMetadata struct {
+	Name  string `json:"name" xml:"name"`
+	Email string `json:"email" xml:"email"`
+}
 
 func main() {
 	cfg := consts.NewConfig()
@@ -15,5 +24,39 @@ func main() {
 	fmt.Printf("METADATA_PATH : %s\n", os.Getenv("METADATA_PATH"))
 	fmt.Printf("REVEAL_SCRIPT_PATH : %s\n", os.Getenv("REVEAL_SCRIPT_PATH"))
 
-	services.NewWatcher(cfg).Watch()
+	go services.NewWatcher(cfg).Watch()
+
+	e := echo.New()
+	e.GET("/metadata/:id", func(c echo.Context) error {
+		id := c.Param("id")
+		tokenID, err := strconv.Atoi(id)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusNotFound, "Not Found.")
+		}
+
+		if tokenID < 1 || tokenID > 10000 {
+			return echo.NewHTTPError(http.StatusNotFound, "Not Found.")
+		}
+
+		str := fmt.Sprintf(`{
+			"name": "CHIBI #%d",
+			"description": "A collection of 10,000 NFTs minted on the Ethereum blockchain. Every character is created by a random algorithm which ensures that each character is unique and differentiated from the others.",
+			"image": "ipfs://QmdttDSCMFk3o6vseaNGjimQ2VFCfkcqwdv2uvjAmNQKxC",
+			"animation_url": "ipfs://QmSUoZUxAfjfXoF5aMYL1GuAPDrHGGjr72CdNB4Bni672Q",
+			"external_url": "https://chibiverse.fun",
+			"creator": "Maxvoy",
+			"attributes": [
+			  {
+				"trait_type": "CHIBI #%d",
+				"value": "?"
+			  }
+			]
+		  }`, tokenID, tokenID)
+
+		var obj map[string]interface{}
+		json.Unmarshal([]byte(str), &obj)
+
+		return c.JSON(http.StatusOK, obj)
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/ethereum/go-ethereum v1.10.15
+	github.com/labstack/echo/v4 v4.2.1
 	github.com/miguelmota/go-ethereum-hdwallet v0.1.1
 	github.com/prongbang/callx v1.2.3
 	github.com/shopspring/decimal v1.3.1
@@ -19,13 +20,18 @@ require (
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/jackpal/go-nat-pmp v1.0.2 // indirect
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/rjeczalik/notify v0.9.1 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/tklauser/go-sysconf v0.3.5 // indirect
 	github.com/tklauser/numcpus v0.2.2 // indirect
 	github.com/tyler-smith/go-bip39 v1.0.1-0.20181017060643-dbb3b84ba2ef // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
 	golang.org/x/sys v0.0.0-20211025112917-711f33c9992c // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
